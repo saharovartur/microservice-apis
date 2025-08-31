7647fe1efaf1cedaedde37ebdcc9fa05db183a63
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2025-08-31 19:40:35 +0400

    в декоратор был добавлен response_model для валидации входящих данных с помощью модели

--- orders/api/api.py
+++ orders/api/api.py
@@ -4,32 +4,33 @@ from starlette.responses import Response
 from starlette import status
 
 from orders.app import app
-from orders.api.schemas import CreateOrderSchema
+from orders.api.schemas import GetOrderSchema, CreateOrderSchema, GetOrdersSchema
 
 order = {
     "id": "f47ac10b-58cc-4372-a567-0e02b2c3d479",
     "status": "delivered",
-    "created": datetime.utcnow(),
+    "created": 'asdf',
+    'updated': datetime.utcnow(),
     "order": [{"product": "coffe", "size": "medium", "quantity": 1}],
 }
 
 
-@app.get("/orders")
+@app.get("/orders", response_model=GetOrdersSchema)
 def get_orders():
     return {"orders": [order]}
 
 
-@app.post("/orders", status_code=status.HTTP_201_CREATED)
+@app.post("/orders", status_code=status.HTTP_201_CREATED, response_model=GetOrderSchema)
 def create_order(order_details: CreateOrderSchema):
     return order
 
 
-@app.get("/orders/{order_id}")
+@app.get("/orders/{order_id}", response_model=GetOrderSchema)
 def get_order(order_id: UUID):
     return order
 
 
-@app.put("/orders/{order_id}")
+@app.put("/orders/{order_id}", response_model=GetOrderSchema)
 def update_order(order_id: UUID, order_detail: CreateOrderSchema):
     return order
 
@@ -39,12 +40,12 @@ def delete_order(order_id: UUID):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.post("orders/{order_id}/cancel")
+@app.post("orders/{order_id}/cancel", response_model=GetOrderSchema)
 def cancel_order(order_id: UUID):
     return order
 
 
-@app.post("/orders/{order_id}/pay")
+@app.post("/orders/{order_id}/pay", response_model=GetOrderSchema)
 def pay_order(order_id: UUID):
     return order
 
