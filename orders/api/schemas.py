1321b4ad9a47ef42dd06a34f69a96ef0bc7e6283
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2025-08-31 18:16:19 +0400

    Добавлен валидатор поля quantity, он не позволит передать 0 в это поле

--- orders/api/schemas.py
+++ orders/api/schemas.py
@@ -3,27 +3,33 @@ from enum import Enum
 from typing import List, Optional
 from uuid import UUID
 
-from pydantic import BaseModel, conlist, conint
+from pydantic import BaseModel, conlist, conint, field_validator
 
 
 class Size(Enum):
-    small = 'small'
-    medium = 'medium'
-    big = 'big'
+    small = "small"
+    medium = "medium"
+    big = "big"
 
 
 class Status(Enum):
-    created = 'created'
-    progress = 'progress'
-    cancelled = 'cancelled'
-    dispatched = 'dispatched'
-    delivered = 'delivered'
+    created = "created"
+    progress = "progress"
+    cancelled = "cancelled"
+    dispatched = "dispatched"
+    delivered = "delivered"
 
 
 class OrderItemSchema(BaseModel):
     product: str
     size: Size
     quantity: Optional[conint(ge=1, strict=True)] = 1
+
+    @field_validator("quantity")
+    def quantity_non_nullable(cls, value):
+        if value is None:
+            raise ValueError("Количество не может быть равным нулю")
+        return value
 
 
 class CreateOrderSchema(BaseModel):
