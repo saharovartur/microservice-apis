671d8496df640d939f0bbbc56159050abd642377
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2025-08-31 14:37:28 +0400

    реализованы первичные модели валидации

---
+++ orders/api/schemas.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, conlist, conint
+
+
+class Size(Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class Status(Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1, strict=True)] = 1
+
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema, min_length=1)
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: Status
+
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]
